2511bead6bab0402e59237344530e42ad5164020
eunjeong <eunjeong0911@naver.com>
2025-10-25 11:54:49 +0900

    Remove: 필요없는 파일 삭제

--- rag_pipline/run.py
+++
@@ -1,8 +0,0 @@
-# input에 따른 output 전체 실행
-from graph.insurance_graph import build_insurance_graph
-
-if __name__ == "__main__":
-    graph = build_insurance_graph()
-    query = input("질문을 입력하세요: ")
-    result = graph.invoke({"user_input": query})
-    print("\n💬 결과:\n", result.get("final_answer"))
